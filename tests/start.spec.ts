b35e3cdb9af088bc1e74d1c2a1f2ea2d517de5b4
wladekkow <wladekkow@gmail.com>
2025-11-05 20:38:12 +0100

    pierwszy push

--- tests/start.spec.ts
+++ tests/start.spec.ts
@@ -19,12 +19,48 @@ test('powinien zalogować użytkownika', async ({ page }) => {
 
 //   await page.click('div[class=bm-burger-button]');
 //   await page.getByRole('button', { name: 'Open Menu' }).click();
-
-  await page.getByRole('button', { name: 'ADD TO CART' }).nth(0).click();
-
-  await page.waitForTimeout(10000);
-
 //   await page.click('a[id="logout_sidebar_link"]');
+
+  await page.getByRole('button', { name: 'ADD TO CART' }).nth(0).click(); //znajdź buttony które mają name = add to cart i klikj pierwszy
+
+  await page.locator('.inventory_item', { has: page.locator('.inventory_item_name', { hasText: 'Sauce Labs Onesie' }) }).locator('button', { name: 'ADD TO CART' }).click();
+
+ await page.click('.shopping_cart_link');
+
+ await page.locator('.cart_item', { has: page.locator('.inventory_item_name', { hasText: 'Sauce Labs Backpack' }) }).locator('button', { name: 'Remove' }).click();
+
+ await page.click('button[name="checkout"]');
+
+ await page.fill('[name="firstName"]', 'Marcin');
+
+ await page.fill('[name="lastName"]', 'Piotrowski');
+
+ await page.fill('[name="postalCode"]', '01-905');
+
+ await page.click('input[name="continue"]');
+
+ await expect(page.locator('.cart_item')).toHaveCount(1);
+
+ await expect(page.locator('.summary_subtotal_label')).toContainText('Item total: $7.99');
+ 
+ await page.click('button[name="finish"]');
+ 
+ await expect(page.locator('.complete-header')).toContainText('Thank you for your order!');
+
+ await page.locator('button:has-text("Back Home")').click();
+ 
+ await expect(page.getByText('Swag Labs')).toBeVisible();
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ await page.waitForTimeout(5000);
+
+
 
 //   await page.waitForTimeout(3000);
 });
